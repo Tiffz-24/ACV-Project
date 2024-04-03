b17dc9e9c807741de7cd82f6126aea873b93058c
Costanza Siniscalchi <cs4014@columbia.edu>
2024-04-03 15:28:10 -0400

    trying to fix train

--- dataloading.py
+++ dataloading.py
@@ -2,12 +2,15 @@ import numpy as np
 import h5py
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import torch
+from PIL import Image
 
 class HEDataset(Dataset):
-    def __init__(self, images_file_path, labels_file_path, transform=None):
+    def __init__(self, images_file_path, labels_file_path, transform=None, crop = None):
         self.images_file_path = images_file_path
         self.labels_file_path = labels_file_path
         self.transform = transform
+        self.crop = crop
         
         # Load the file to check the shape of the dataset
         self._print_dataset_shapes()
@@ -15,16 +18,18 @@ class HEDataset(Dataset):
     def _print_dataset_shapes(self):
         with h5py.File(self.images_file_path, 'r') as images_file:
             # Assuming you want to know the shape of the first image dataset
+            
             first_image_key = list(images_file.keys())[0]
             image_shape = images_file[first_image_key].shape
-            print(f"Image dataset shape: {image_shape}")
             self.length = image_shape[0]
-        
+            if self.crop:
+                print(f"Image shape {image_shape} will crop to ", [self.length, 3, self.crop, self.crop])
+            else:
+                print(f"Image shape {[self.length, 3, image_shape[2], image_shape[3]]}")
         with h5py.File(self.labels_file_path, 'r') as labels_file:
             # Assuming you want to know the shape of the first label dataset
             first_label_key = list(labels_file.keys())[0]
             label_shape = labels_file[first_label_key].shape
-            print(f"Label dataset shape: {label_shape}")
 
             if label_shape[0] != self.length:
                 print("BAD: make error. Dataset x and y sizes do not match")
@@ -34,30 +39,43 @@ class HEDataset(Dataset):
 
     def __getitem__(self, idx):
         with h5py.File(self.images_file_path, 'r') as images_file:
-            image_name = list(images_file.keys())[idx]
-            image_data = images_file[image_name][()]
+            first_image_key = list(images_file.keys())[0]
+            image_data = list(images_file[first_image_key])[idx]
+            if image_data.shape[-1] == 3:
+                image_data = image_data.transpose((2, 0, 1))  # RGB filter in 0 position
 
         with h5py.File(self.labels_file_path, 'r') as labels_file:
-            label_data = labels_file[image_name][()]
+            first_label_key = list(labels_file.keys())[0]
+            label_data = list(labels_file[first_label_key])[idx]
+        print("got image and label data")
+        if self.crop:
+            w, h = image_data.shape[1], image_data.shape[2]
+            startx = w // 2 - self.crop // 2
+            starty = h // 2 - self.crop // 2
+            image_data = image_data[:, starty:starty + self.crop, startx:startx + self.crop]
+            print("cropped to ", image_data.shape)
+
+        # Convert to PIL image using PIL Image module
+        image_data = Image.fromarray(np.uint8(image_data.transpose(1, 2, 0)))
 
         if self.transform:
             image_data = self.transform(image_data)
+            print("applied transform")
 
-        return image_data, np.array(label_data, dtype=np.float32)
+        return image_data, torch.tensor(label_data, dtype=torch.float32)
 
-def load_datasets(test_x,test_y, train_x, train_y, valid_x,valid_y):
+def load_datasets(test_x,test_y, train_x, train_y, valid_x,valid_y, transform=None, crop = None):
 
-    test_dataset = HEDataset(test_x, test_y)
-    train_dataset = HEDataset(train_x, train_y)
-    validation_dataset = HEDataset(valid_x, valid_y)
+    test_dataset = HEDataset(test_x, test_y, transform, crop)
+    train_dataset = HEDataset(train_x, train_y, transform, crop)
+    validation_dataset = HEDataset(valid_x, valid_y, transform, crop)
 
     return test_dataset, train_dataset, validation_dataset
 
 def load_dataloaders(test_dataset, train_dataset, validation_dataset, batch_size = 64, shuffle = True):
-    BATCH_SIZE = batch_size
     
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=shuffle)
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=shuffle)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=shuffle)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=shuffle)
     
     return test_dataloader, train_dataloader, validation_dataloader
--- resnet.py
+++ resnet.py
@@ -4,57 +4,63 @@ import torch.nn.functional as F
 class ResNetBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding = 1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding = 1)
-        self.skip_connection = nn.Identity()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.skip_connection = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
-          #Use a Conv2d layer with kernel_size=1 to "resize" input
-          self.skip_connection = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False)
+            self.skip_connection = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
 
     def forward(self, x):
-        identity_x = self.skip_connection(x)
-        identity_x = self.conv1(identity_x)
-        identity_x = F.relu(identity_x)
-        identity_x = self.conv2(identity_x)
-        x = x + identity_x
-        return x
+        print("RESNET BLOCK FORWARD: ", x.shape)
+        identity = self.skip_connection(x)
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        out += identity
+        out = F.relu(out)
+        return out
 
 class ResNet(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels=3, num_classes=10):
         super().__init__()
-        # Need to crop the input to focus on the central 32x32 region
-        # start off with 64 3x32x32 images
-        # need to adjust the numbers for this dataset
-        self.conv1 = nn.Conv2d(3, 32, 9) # 64x32x24x24
-        self.pool1 = nn.MaxPool2d(2) # 64x32x12x12
-        self.ResNet1 = ResNetBlock(32, 64, stride=2) # 64x64x12x12
-        self.ResNet2 = ResNetBlock(64, 128) # 64x128x12x12
-
-        self.conv2 = nn.Conv2d(128, 256, 5) # 64x256x8x8
-        self.conv3 = nn.Conv2d(256, 256, 5) # 64x256x4x4
-        self.pool2 = nn.MaxPool2d(2) # 64x256x2x2
-
-        self.flatten = nn.Flatten()
-        self.lin1 = nn.Linear(1024, 512)
-        self.lin2 = nn.Linear(512, 100)
-        self.lin3 = nn.Linear(100, 10)
+        self.conv1 = nn.Conv2d(input_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        # Define ResNet layers
+        self.layer1 = ResNetBlock(64, 64)
+        self.layer2 = ResNetBlock(64, 128, stride=2)
+        self.layer3 = ResNetBlock(128, 256, stride=2)
+        self.layer4 = ResNetBlock(256, 512, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # This makes the network input size agnostic
+        self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
+        print("FORWARD: ", x.shape)
         x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool1(x)
-        x = self.ResNet1(x)
-        x = self.ResNet2(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = self.pool2(x)
-        x = self.flatten(x)
-        x = self.lin1(x)
-        x = F.relu(x)
-        x = self.lin2(x)
-        x = F.relu(x)
-        x = self.lin3(x)
-        return x
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)  # Adaptive pooling
+        x = x.reshape(x.size(0), -1)
+        x = self.fc(x)
+        return x
