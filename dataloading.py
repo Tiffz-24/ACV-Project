508ea0219aa132f590bcd9bb992d466d14c39f6a
Costanza Siniscalchi <cs4014@columbia.edu>
2024-04-20 17:29:44 -0400

    new dataloading

--- dataloading.py
+++ dataloading.py
@@ -5,77 +5,25 @@ from torchvision import transforms
 import torch
 from PIL import Image
 
-class HEDataset(Dataset):
-    def __init__(self, images_file_path, labels_file_path, transform=None, crop = None):
-        self.images_file_path = images_file_path
-        self.labels_file_path = labels_file_path
+class PCamDataset(torch.utils.data.Dataset):
+    def __init__(self, examples, transform=None):
+        self.examples = examples
         self.transform = transform
-        self.crop = crop
-        
-        # Load the file to check the shape of the dataset
-        self._print_dataset_shapes()
-    
-    def _print_dataset_shapes(self):
-        with h5py.File(self.images_file_path, 'r') as images_file:
-            # Assuming you want to know the shape of the first image dataset
-            
-            first_image_key = list(images_file.keys())[0]
-            image_shape = images_file[first_image_key].shape
-            self.length = image_shape[0]
-            if self.crop:
-                print(f"Image shape {image_shape} will crop to ", [self.length, 3, self.crop, self.crop])
-            else:
-                print(f"Image shape {[self.length, 3, image_shape[2], image_shape[3]]}")
-        with h5py.File(self.labels_file_path, 'r') as labels_file:
-            # Assuming you want to know the shape of the first label dataset
-            first_label_key = list(labels_file.keys())[0]
-            label_shape = labels_file[first_label_key].shape
 
-            if label_shape[0] != self.length:
-                print("BAD: make error. Dataset x and y sizes do not match")
+    def __getitem__(self, index):
+        image_fp, label = self.examples[index]
+        image = Image.open(image_fp)
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, torch.Tensor([label]).long()
 
     def __len__(self):
-        return self.length
+        return len(self.examples)
 
-    def __getitem__(self, idx):
-        with h5py.File(self.images_file_path, 'r') as images_file:
-            first_image_key = list(images_file.keys())[0]
-            image_data = list(images_file[first_image_key])[idx]
-            if image_data.shape[-1] == 3:
-                image_data = image_data.transpose((2, 0, 1))  # RGB filter in 0 position
-
-        with h5py.File(self.labels_file_path, 'r') as labels_file:
-            first_label_key = list(labels_file.keys())[0]
-            label_data = list(labels_file[first_label_key])[idx]
-        print("got image and label data")
-        if self.crop:
-            w, h = image_data.shape[1], image_data.shape[2]
-            startx = w // 2 - self.crop // 2
-            starty = h // 2 - self.crop // 2
-            image_data = image_data[:, starty:starty + self.crop, startx:startx + self.crop]
-            print("cropped to ", image_data.shape)
-
-        # Convert to PIL image using PIL Image module
-        image_data = Image.fromarray(np.uint8(image_data.transpose(1, 2, 0)))
-
-        if self.transform:
-            image_data = self.transform(image_data)
-            print("applied transform")
-
-        return image_data, torch.tensor(label_data, dtype=torch.float32)
-
-def load_datasets(test_x,test_y, train_x, train_y, valid_x,valid_y, transform=None, crop = None):
-
-    test_dataset = HEDataset(test_x, test_y, transform, crop)
-    train_dataset = HEDataset(train_x, train_y, transform, crop)
-    validation_dataset = HEDataset(valid_x, valid_y, transform, crop)
-
-    return test_dataset, train_dataset, validation_dataset
-
-def load_dataloaders(test_dataset, train_dataset, validation_dataset, batch_size = 64, num_workers = 4, shuffle = True):
+# def load_dataloaders(test_dataset, train_dataset, validation_dataset, batch_size = 64, num_workers = 4, shuffle = True):
     
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+#     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+#     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+#     validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     
-    return test_dataloader, train_dataloader, validation_dataloader
+#     return test_dataloader, train_dataloader, validation_dataloader
--- train.py
+++ train.py
@@ -1,45 +1,34 @@
 import torch
 from torch.nn import CrossEntropyLoss
 from torch.optim import Adam
+import logging as log
+from tqdm.notebook import tqdm
 
-def train_network(net, trainloader, criterion=CrossEntropyLoss(), optimizer_class=Adam, n_epochs=50):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("Device: ", device)
+def train(model, epoch, dataloader, criterion, optimizer, scheduler=None):
+    model.train()
+    total, total_loss, total_correct = 0, 0., 0.
 
-    net.to(device)
-    
-    # Instantiate the criterion if it's a class.
-    if isinstance(criterion, type):
-        criterion = criterion()
-    
-    # Instantiate the optimizer with the network parameters.
-    optimizer = optimizer_class(net.parameters())
-
-    for epoch in range(n_epochs):  # loop over the dataset multiple times
-        print("starting epoch ", epoch)
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-            print("got data")
-            inputs, labels = inputs.to(device), labels.to(device)
-            print("inputs and labels to device")
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            print("zero grad")
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-            # print statistics
-            if i % 1000 == 0 and i > 0:
-                print(f'Epoch={epoch + 1} Iter={i + 1:5d} Loss={running_loss / 1000:.3f}')
-                running_loss = 0.0
-
-    print('Finished Training')
-    return net
+    tqdm_iterator = tqdm(enumerate(dataloader), total=len(dataloader))
+    for i, (x, y) in tqdm_iterator:
+        if torch.cuda.is_available():
+            x, y = x.cuda(), y.cuda()
+        optimizer.zero_grad()
+        output = model(x)
+        prediction = torch.argmax(output, -1)
+        loss = criterion(output, y.squeeze())
+        total_loss += loss.item() * len(y)
+        total_correct += (prediction == y.squeeze()).sum().item()
+        total += len(y)
+        loss.backward()
+        optimizer.step()
+        if scheduler is not None:
+            scheduler.step()
+        tqdm_iterator.set_postfix({
+            "Loss": (total_loss / total), "Accuracy": (total_correct / total)
+        })
+    final_loss, final_acc = total_loss / total, total_correct / total
+    log.info(
+        "Reporting %.5f training loss, %.5f training accuracy for epoch %d." %
+        (final_loss, final_acc, epoch)
+    )
+    return final_loss, final_acc, model
